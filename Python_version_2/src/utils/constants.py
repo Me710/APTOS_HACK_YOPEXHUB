7b4e6f2a91e75aa2bf4728d0fe89a0268c8cab18
Me710 <christianparfait.nebotfonkou@esprit.tn>
2024-07-27 10:46:03 +0100

    Update Python

--- Python_version_2/src/utils/constants.py
+++
@@ -1,4 +0,0 @@
-NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1"
-FAUCET_URL = "https://faucet.devnet.aptoslabs.com"
-YOPEX_MODULE_ADDRESS = "0x9cfb5f869b27d71d882524530aecdbe4e40d94006cccd41dd373f34f6acf51ae"
-YOPEX_MODULE_NAME = "yopex"
